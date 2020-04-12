464cba65ffa2ed4877c0927a612b7d41f4c09ab2
JonasEnglund <klop9054@gmail.com>
2020-04-12 05:23:41 +0300

    New

---
+++ Vector/MyVector.cpp
@@ -0,0 +1,208 @@
+#include "MyVector.h"
+#include <iostream>
+#include <cmath>
+
+MyVector::MyVector(size_t size, ValueType value, ResizeStrategy strategy, float coef) :
+_size(size), _strategy(strategy), _coef(coef) {
+    if(_size == 0) {
+        this->_capacity = 1;
+    } else
+        this->_capacity = std::ceil(_size*_coef);
+
+    _data = new ValueType[_capacity];
+
+    for (size_t i = 0; i < _size; ++i) {
+        _data[i] = value;
+    }
+}
+
+MyVector::MyVector(size_t size, ResizeStrategy strategy, float coef) : _size(size), _capacity(1), _strategy(strategy), _coef(coef) {
+    this->_data = new ValueType[_capacity];
+}
+
+MyVector::~MyVector() {
+    delete[] _data;
+    this->_data = nullptr;
+}
+
+size_t MyVector::capacity() const {
+    return this->_capacity;
+}
+
+size_t MyVector::size() const {
+    return this->_size;
+}
+
+float MyVector::loadFactor(size_t newSize) {
+    return float(newSize)/_capacity;
+}
+
+ValueType &MyVector::operator[](const size_t i) const {
+    if(_size < i){
+        std::cout << "Error: index > size" << std::endl;
+        return _data[0];
+    } else
+    return _data[i];
+}
+
+MyVector::MyVector(const MyVector &copy) {
+    this->_size = copy._size;
+    this->_capacity = copy._capacity;
+    this->_strategy = copy._strategy;
+    this->_coef = copy._coef;
+    this->_data = nullptr;
+    memcpy(_data, copy._data, _size * sizeof(ValueType));
+}
+
+
+void MyVector::reserve(const size_t capacity) {
+    if(_size < capacity) {
+        ValueType *newData = new ValueType[capacity];
+        memcpy(newData, _data, _size * sizeof(ValueType));
+        delete[] _data;
+        this->_data = newData;
+        this->_capacity = capacity;
+    }
+    else if(_size > capacity) {
+        ValueType *tempdata = new ValueType[capacity];
+        memcpy(tempdata, _data, capacity * sizeof(ValueType));
+        delete[] _data;
+        this->_data = tempdata;
+        this->_size = capacity;
+        this->_capacity = capacity;
+    }
+}
+
+void MyVector::insert(const size_t index, const ValueType &value) {
+    if(index > _size) {
+        std::cout << "Error: index > size";
+        return;
+    }
+    if(_size == _capacity) {
+        reserve(_capacity + 1);
+        for(size_t j = _size; j > index; --j)
+            _data[j] = _data[j - 1];
+        this->_data[index] = value;
+        ++this->_size;
+    }
+    else {
+        for(size_t j = _size; j > index; --j)
+            _data[j] = _data[j - 1];
+        this->_data[index] = value;
+        ++this->_size;
+    }
+}
+
+void MyVector::insert(const size_t i, const MyVector &value) {
+    for(size_t j = value.size(); j > 0; --j)
+        this->insert(i, value[j]);
+}
+
+MyVector &MyVector::operator=(const MyVector &copy) {
+    this->_size = copy._size;
+    this->_capacity = copy._capacity;
+    this->_strategy = copy._strategy;
+    this->_coef = copy._coef;
+    memcpy(this->_data, copy._data, _size * sizeof(ValueType));
+    return *this;
+}
+
+void MyVector::pushBack(const ValueType &value) {
+    this->insert(this->_size, value);
+}
+
+void MyVector::clear() {
+    delete[] _data;
+    this->_data= new ValueType[_capacity];
+}
+
+void MyVector::popBack() {
+    ValueType* newData = new ValueType[_capacity];
+    memcpy(newData, _data, sizeof(ValueType)* (_size - 1));
+    delete[] _data;
+    this->_data = newData;
+    --this->_size;
+}
+void MyVector::erase(const size_t i) {
+    for(size_t j = i; j < _size - 1; ++j)
+        this->_data[j] = this->_data[j + 1];
+    popBack();
+}
+
+void MyVector::erase(const size_t i, const size_t len) {
+    for(size_t j = 0; j < len; ++j)
+        this->erase(i);
+}
+
+long long int MyVector::find(const ValueType &value, bool isBegin) const {
+    if (isBegin) {
+        for (size_t i = 0; i < _size; ++i)
+            if (_data[i] == value)
+                return i;
+    }
+    else {
+        for (size_t i = _size - 1; i > 0; --i)
+            if (_data[i] == value)
+                return i;
+    }
+    return -1;
+}
+void MyVector::resize(const size_t size) {
+    if(loadFactor(size) > 1) {
+        _capacity = size;
+        _capacity *= std::ceil(_coef);
+
+        ValueType* newData = new ValueType[_capacity];
+        memcpy(newData, _data, size * sizeof(ValueType));
+        delete[] _data;
+        this->_data = newData;
+
+        for(size_t i = _size; i < size; ++i){
+            _data[i] = 0.0;
+        }
+        this->_size = size;
+    }
+    else if(loadFactor(size) < 1/(_coef * _coef)) {
+        _capacity *= 0.5;
+        ValueType* newData = new ValueType[_capacity];
+        memcpy(newData, _data, size * sizeof(ValueType));
+        delete[] _data;
+        this->_data = newData;
+        this->_size = size;
+    }
+    else {
+        for(size_t i = _size; i < size; ++i){
+            _data[i] = 0.0;
+        }
+        this->_size = size;
+    }
+}
+
+MyVector MyVector::sortedSquares(const MyVector &vec, SortedStrategy strategy) {
+    for(size_t i = 0; i < vec.size(); ++i){
+        vec[i] *= vec[i];
+    }
+    switch(strategy){
+        case (SortedStrategy::Rising):
+            for(size_t j = 1; j < vec.size(); ++j)
+            {
+                for (size_t i = 0; i < vec.size() - j; ++i) {
+                    if (vec[i] > vec[i + 1]) {
+                        std::swap(vec[i], vec[i + 1]);
+                    }
+                }
+            }
+            break;
+        case (SortedStrategy::Decrising):
+            for(size_t j = 1; j < vec.size(); ++j)
+            {
+                for (size_t i = 0; i < vec.size() - j; ++i) {
+                    if (vec[i] < vec[i + 1]) {
+                        std::swap(vec[i], vec[i + 1]);
+                    }
+                }
+            }
+            break;
+    }
+    return MyVector();
+}
--- Vector/MyVector.h
+++ Vector/MyVector.h
@@ -1,71 +1,95 @@
 #pragma once
+#ifndef CLIONPROJECTS_MYVECTOR_H
+#define CLIONPROJECTS_MYVECTOR_H
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
 
 // стратегия изменения capacity
 enum class ResizeStrategy {
-	Additive,
-	Multiplicative
+    Additive,
+    Multiplicative
 };
 
-// тип значений в векторе
-// потом будет заменен на шаблон
+enum class SortedStrategy {
+    Rising,
+    Decrising
+};
+
 using ValueType = double;
 
 class MyVector
 {
 public:
-	MyVector(size_t size = 0, ResizeStrategy = ResizeStrategy::Multiplicative, float coef = 1.5f);
-	MyVector(size_t size, ValueType value, ResizeStrategy = ResizeStrategy::Multiplicative, float coef = 1.5f);
-	
-	MyVector(const MyVector& copy);
-	MyVector& operator=(const MyVector& copy);
 
-	~MyVector();
+    MyVector (size_t size = 0, ResizeStrategy = ResizeStrategy::Multiplicative, float coef = 1.5f);
+    MyVector (size_t size, ValueType value, ResizeStrategy = ResizeStrategy::Multiplicative, float coef = 1.5f);
 
-	// для умненьких — реализовать конструктор и оператор для перемещения
+    MyVector (const MyVector& copy);
+    MyVector& operator= (const MyVector& copy);
 
-	size_t capacity() const;
-	size_t size() const;
-	float loadFactor();
+    ~MyVector();
 
-	// доступ к элементу, 
-	// должен работать за O(1)
-	ValueType& operator[](const size_t i) const;
+    size_t capacity() const;
+    size_t size() const;
+    float loadFactor(size_t size);
+    ValueType& operator[](const size_t i) const;
 
-	// добавить в конец,
-	// должен работать за amort(O(1))
-	void pushBack(const ValueType& value);
-	// вставить,
-	// должен работать за O(n)
-	void insert(const size_t i, const ValueType& value);	// версия для одного значения
-	void insert(const size_t i, const MyVector& value);		// версия для вектора
+    void pushBack(const ValueType& value);
+    void insert(const size_t i, const ValueType& value);
+    void insert(const size_t i, const MyVector& value);
 
-	// удалить с конца,
-	// должен работать за amort(O(1))
-	void popBack();
-	// удалить
-	// должен работать за O(n)
-	void erase(const size_t i);
-	void erase(const size_t i, const size_t len);			// удалить len элементов начиная с i
+    void popBack();
+    void erase(const size_t i);
+    void erase(const size_t i, const size_t len);
 
-	// найти элемент,
-	// должен работать за O(n)
-	// если isBegin == true, найти индекс первого элемента, равного value, иначе последнего
-	// если искомого элемента нет, вернуть -1
-	long long int find(const ValueType& value, bool isBegin = true) const;	
+    long long int find(const ValueType& value, bool isBegin = true) const;
 
-	// зарезервировать память (принудительно задать capacity)
-	void reserve(const size_t capacity);
+    void reserve(const size_t capacity);
 
-	// изменить размер
-	// если новый размер больше текущего, то новые элементы забиваются дефолтными значениями
-	// если меньше - обрезаем вектор
-	void resize(const size_t size, const ValueType = 0.0);
+    void resize(const size_t size);
 
-	// очистка вектора, без изменения capacity
-	void clear();
+    MyVector sortedSquares(const MyVector& vec, SortedStrategy strategy);
+    void clear();
+
+    class Iterator {
+
+    public:
+        Iterator(ValueType *k) : _value(k) {};
+
+        Iterator(const Iterator &it) : _value(it._value) {};
+
+        Iterator operator+(int n) { return Iterator(_value + n); }
+
+        Iterator operator-(int n) { return Iterator(_value - n); }
+
+        Iterator operator++() { return ++_value; }
+
+        Iterator operator--() { return --_value; }
+
+        Iterator operator++(int) { return _value++; }
+
+        Iterator operator--(int) { return _value--; }
+
+        bool operator!=(const Iterator &it) const { return _value != it._value; }
+
+        bool operator==(const Iterator &it) const { return _value == it._value; }
+
+        ValueType &operator*() { return *_value; }
+
+    private:
+        ValueType *_value;
+    };
+
+    Iterator begin() { return Iterator(_data); };
+    Iterator end() { return Iterator(_data + _size); };
+
 private:
-	ValueType* _data;
-	size_t _size;
-	size_t _capacity;
+    ValueType* _data;
+    size_t _size;
+    size_t _capacity;
+    ResizeStrategy _strategy;
+    float _coef;
 };
 
+#endif //CLIONPROJECTS_MYVECTOR_H
